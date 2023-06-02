98b59bc88bd52a7a1845093510cb98048370ec12
maksim.prudnikau <maksim.prudnikau@devstream,by>
2023-06-02 09:57:07 +0300

    add vocabulary input field validation

--- src/components/Translations/TranslationInput.tsx
+++ src/components/Translations/TranslationInput.tsx
@@ -1,4 +1,12 @@
-import { Dispatch, FC, Fragment, SetStateAction } from "react";
+import {
+  ChangeEvent,
+  Dispatch,
+  FC,
+  Fragment,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import { EnterInput } from "../Shared/EnterInput.tsx";
 
 interface IProps {
@@ -7,14 +15,44 @@ interface IProps {
   onBlur: () => Promise<void>;
 }
 
-export const TranslationInput: FC<IProps> = ({ value, setter, onBlur }) => {
+export const TranslationInput: FC<IProps> = (props) => {
+  const { value, setter } = props;
+  const [touched, setTouched] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const validate = validateValue(value);
+    setError(validate);
+  }, [value]);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTouched(true);
+    const trim = e.target.value.trim();
+    setter(trim);
+  };
+
+  const onBlur = async () => {
+    if (error) return;
+
+    await props.onBlur();
+  };
+
   return (
     <Fragment>
-      <EnterInput
-        value={value}
-        onChange={(e) => setter(e.target.value)}
-        onBlur={onBlur}
-      />
+      <EnterInput value={value} onChange={onChange} onBlur={onBlur} />
+      {touched && error && <label style={{ color: "red" }}>{error}</label>}
     </Fragment>
   );
 };
+
+function validateValue(value: string | undefined | null): string | null {
+  if (!value || value.length === 0) {
+    return "Required";
+  }
+
+  if (value.length > 255) {
+    return "Too long";
+  }
+
+  return null;
+}
